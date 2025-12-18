3eec7fc11094c8a2c31523c8b743154995ab6969
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-12-18 23:38:45 +0530

    code update

--- Custom-Annotation/src/main/java/interfaces/Info.java
+++ Custom-Annotation/src/main/java/interfaces/Info.java
@@ -1,12 +1,11 @@
 package interfaces;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Inherited
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Info {
     String author();
     int version();
---
+++ Dependency-Injection/src/main/java/demo/Demo.java
@@ -0,0 +1,44 @@
+package demo;
+
+import interfaces.Engine;
+
+class PetrolEngine implements Engine {
+    @Override
+    public void start() {
+        System.out.println("Petrol Engine");
+    }
+}
+
+class ElectricEngine implements Engine {
+    @Override
+    public void start() {
+        System.out.println("Electric Engine");
+    }
+}
+
+class Car {
+
+    private Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    void drive() {
+        engine.start();
+    }
+
+}
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Engine engine = new ElectricEngine();
+        Car car = new Car(engine);
+        car.drive();
+
+
+    }
+
+}
